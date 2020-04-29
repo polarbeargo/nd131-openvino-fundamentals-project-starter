1d8fc72f4ad28951e67579215d907e88998e98eb
alice <bow1226@gmail.com>
2020-04-29 10:35:30 +0800

    feat: Send current_count, total_count and duration topic to MQTT server
    Send current_count, total_count and duration
    Topic "person": keys of "count" and "total"
    Topic "person/duration": key of "duration"
     to MQTT server .

--- main.py
+++ main.py
@@ -98,14 +98,14 @@ def infer_on_stream(args, client):
     net_input_shape = infer_network.get_input_shape()
 
     # Handle the input stream ###
-    if args.input =='CAM':
+    if args.input == 'CAM':
         input_stream = 0
         single_image_mode = False
     elif args.input[-4:] in [".jpg", ".bmp", ".png"]:
         single_image_mode = True
         input_stream = args.input
     else:
-        single_image_mode=False
+        single_image_mode = False
         input_stream = args.input
         assert os.path.isfile(input_stream)
 
@@ -113,34 +113,52 @@ def infer_on_stream(args, client):
     cap.open(input_stream)
     if not cap.isOpened():
         log.error("Unable open video stream")
-    logger.debug( "Weight-Height: " + str(capture.get(cv2.CAP_PROP_FRAME_WIDTH)) + "-" + str(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    
+    logger.debug("Weight-Height: " + str(capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+                                         ) + "-" + str(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    data_list = []
+    total_count = 0
+    current_count = 0
+    request_id = 0
+    num_detected = 0
+
     # Loop until stream is over ###
     while cap.isOpened():
         ### TODO: Read from the video capture ###
         flag, frame = cap.read()
         if not flag:
             break
-         request_id = 0
+
         # Pre-process the image as needed ###
         logger.debug("size: ".format(net_input_shape))
         p_frame = cv2.resize(frame, (net_input_shape[3], net_input_shape[2]))
         p_frame = p_frame.transpose((2, 0, 1))
         p_frame = p_frame.reshape(net_input_shape)
+
         # Start asynchronous inference for specified request ###
         infer_network.exec_net(request_id, p_frame)
+
         # Wait for the result ###
         if plugin.wait() == 0:
+
             # Get the results of the inference request ###
-            result = infer_network.exec_net(request_id, frame.shape,prob_threshold)
+            result = infer_network.exec_net(
+                request_id, frame.shape, prob_threshold)
 
             # Extract any desired stats from the results ###
-            count, box_frame = count_targets(result,frame)
+            count, box_frame = count_targets(result, frame)
 
             ### TODO: Calculate and send relevant information on ###
             ### current_count, total_count and duration to the MQTT server ###
             ### Topic "person": keys of "count" and "total" ###
             ### Topic "person/duration": key of "duration" ###
+            if total_count > 0:
+                duration = time/total_count
+                client.publish("person/duration",
+                               json.dumps({"duration": int(duration)}))
+
+            client.publish("person", json.dumps(
+                {"count": num_detected}), retain=True)
+
         key_pressed = cv2.waitKey(60)
         if key_pressed == 27:
             write_csv(data_list)
@@ -153,7 +171,7 @@ def infer_on_stream(args, client):
         logger.debug("Image_size: {}".format(box_frame.shape))
         sys.stdout.buffer.write(box_frame)
         sys.stdout.flush()
-    
+
         # Write an output image if `single_image_mode`
         if single_image_mode:
             cv2.imwrite("output.jpg",)
@@ -163,11 +181,13 @@ def infer_on_stream(args, client):
         cv2.destroyAllWindows()
         client.disconnect()
 
+
 def write_csv(data):
-    with open('./log.csv','w') as outfile:
-        writer = DictWriter(outfile,())
+    with open('./log.csv', 'w') as outfile:
+        writer = DictWriter(outfile, ())
         writer.writeheader()
         writer.writerows(data)
+
 
 def count_targets(detections, image):
     num_detections = 0
@@ -176,15 +196,18 @@ def count_targets(detections, image):
         draw_bounding_box, num_detections = draw_boxes(detections, image)
     return num_detections, draw_bounding_box
 
+
 def draw_boxes(boxes, image):
     num_detections = 0
     for box in boxes:
         logger.debug("box: {}".format(box))
         if box['class_id'] == 0:
             if box['confidence'] > 0:
-                cv2.rectangle(image,(box['xmin'], box['ymin']), (box['xmax'], box['ymax']), (0,255,0), 1)
-                num_detections +=1
+                cv2.rectangle(
+                    image, (box['xmin'], box['ymin']), (box['xmax'], box['ymax']), (0, 255, 0), 1)
+                num_detections += 1
     return image, num_detections
+
 
 def main():
     """
