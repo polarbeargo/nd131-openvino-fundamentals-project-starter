4e737c781fca7351b567f175c74b81c4d32b51b0
alice <bow1226@gmail.com>
2020-05-28 10:05:28 +0800

    fix: Upgrade to python3

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ cd tensorflow
 wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 tar xvzf ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 cd ssd_mobilenet_v2_coco_2018_03_29
-python /opt/intel/openvino/deployment_tools/model_optimizer/mo.py --input_model frozen_inference_graph.pb --tensorflow_object_detection_api_pipeline_config pipeline.config --reverse_input_channels --tensorflow_use_custom_operations_config /opt/intel/openvino/deployment_tools/model_optimizer/extensions/front/tf/ssd_v2_support.json
+python3 /opt/intel/openvino/deployment_tools/model_optimizer/mo.py --input_model frozen_inference_graph.pb --tensorflow_object_detection_api_pipeline_config pipeline.config --reverse_input_channels --tensorflow_use_custom_operations_config /opt/intel/openvino/deployment_tools/model_optimizer/extensions/front/tf/ssd_v2_support.json
 cd ../../../
 cd webservice/server
 npm install
