08a691265d71fc799e4f7b5d81722febe9ec5742
alice <bow1226@gmail.com>
2020-06-12 11:26:23 +0800

    chore: Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,6 @@ python3 -m venv venv
 source /opt/intel/openvino/bin/setupvars.sh -pyver 3.6
 source venv/bin/activate
 pip3 install -r requirements.txt
-mkdir models
-python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name pedestrian-detection-adas-0002 -o models/ --precisions FP16
-python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name person-reidentification-retail-0031 -o models/ --precisions FP16
-python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name person-reidentification-retail-0248 -o models/ --precisions FP16
 cd webservice/server
 npm install
 cd ../ui
@@ -21,4 +17,3 @@ cd ../../
 kill $(sudo lsof -t -i:3004)
 ffserver -f ./ffmpeg/server.conf &
 python3 main.py -i resources/Pedestrian_Detect_2_1_1.mp4 -m frozen_inference_graph.xml -d CPU -pt 0.6 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://0.0.0.0:3004/fac.ffm
-
